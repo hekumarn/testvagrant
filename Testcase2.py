9f7d2247fb9758d3fddd193001fa368c0acab4ba
HemanthKumar <41278056+hekumarn@users.noreply.github.com>
2022-11-14 21:18:36 +0530

    Add files via upload

---
+++ Testcase2.py
@@ -0,0 +1,98 @@
+import json
+x = {
+  "name": "Royal Challengers Bangalore",
+  "location": "Bangalore",
+  "player": [
+    {
+      "name": "Faf Du Plessis",
+      "country": "South Africa",
+      "role": "Batsman",
+      "price-in-crores": "7"
+    },
+    {
+      "name": "Virat Kohli",
+      "country": "India",
+      "role": "Batsman",
+      "price-in-crores": "15"
+    },
+    {
+      "name": "Glenn Maxwell",
+      "country": "Australia",
+      "role": "Batsman",
+      "price-in-crores": "11"
+    },
+    {
+      "name": "Mohammad Siraj",
+      "country": "India",
+      "role": "Bowler",
+      "price-in-crores": "7"
+    },
+    {
+      "name": "Harshal Patel",
+      "country": "India",
+      "role": "Bowler",
+      "price-in-crores": "10.75"
+    },
+    {
+      "name": "Wanindu Hasaranga",
+      "country": "Srilanka",
+      "role": "Bowler",
+      "price-in-crores": "10.75"
+    },
+    {
+      "name": "Dinesh Karthik",
+      "country": "India",
+      "role": "Wicket-keeper",
+      "price-in-crores": "5.5"
+    },
+    {
+      "name": "Shahbaz Ahmed",
+      "country": "India",
+      "role": "All-Rounder",
+      "price-in-crores": "2.4"
+    },
+    {
+      "name": "Rajat Patidar",
+      "country": "India",
+      "role": "Batsman",
+      "price-in-crores": "0.20"
+    },
+    {
+      "name": "Josh Hazlewood",
+      "country": "Australia",
+      "role": "Bowler",
+      "price-in-crores": "7.75"
+    },
+    {
+      "name": "Mahipal Lomror",
+      "country": "India",
+      "role": "Bowler",
+      "price-in-crores": "7.75"
+    }
+  ]
+}
+y = json.dumps(x)
+sno = 0
+list=[]
+for i in x:
+  t = x['player']
+  for y in t:
+    role = y['role']
+    Playername = y['name']
+    if role == "Wicket-keeper":
+        list.append(Playername)
+
+  break
+array = [x for x in list if x]
+count = (len(array))
+if count == 1:
+  print("Test case passed")
+else:
+  print("test failed")
+
+
+
+
+
+
+
